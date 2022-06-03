b544c11c4ad006e3b6bb61c143e06c8405deed4c
Gonzalo Rojas <gonzalo.rojas@doctum.net>
2022-06-03 01:59:37 -0400

    Fix web

--- proovedor_api/routes/web.php
+++ proovedor_api/routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'Holas';
+    return 'API - Proveedor';
 });
 
-Route::get('/api/product',[\App\Http\Controllers\ProductController::class,'index']);
-Route::get('/product/{id}',[\App\Http\Controllers\ProductController::class,'show']);
